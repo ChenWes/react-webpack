5760e3adc55305afe122877d2aa93b9d5af0dfee
ANNAHUANG-PC\AnnaHuang <chenxuhua0530@163.com>
2018-04-19 23:29:38 +0800

    启用服务端渲染

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const ReastSSR = require('react-dom/server');
+const fs = require('fs');
+const path = require('path');
+const serverEntry = require('../dist/server-entry').default;
+
+const template = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf8');
+
+const app = new express();
+
+app.use('/public', express.static(path.join(__dirname, '../dist')));
+
+app.get('*', function (req, res) {
+    const appString = ReastSSR.renderToString(serverEntry);
+    res.send(template.replace('<app></app>', appString));
+});
+
+app.listen(3333, function () {
+    console.log('Web Server Listen On 3333')
+})
