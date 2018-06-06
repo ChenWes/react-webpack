5d3ec6b0f7ecd3ad68118e0423d11fad3dca9f47
ANNAHUANG-PC\AnnaHuang <chenxuhua0530@163.com>
2018-06-06 22:48:32 +0800

    使用ESLint检测代码
    yarn add babel-eslint \
    eslint-config-airbnb \
    eslint-config-standard \
    eslint-loader \
    eslint-plugin-import \
    eslint-plugin-jsx-a11y \
    eslint-plugin-node \
    eslint-plugin-promise \
    eslint-plugin-react \
    eslint-plugin-standard -D
    
    使用husky在git每次commit时都做ESLint检测
    
    yarn add husky -D

--- client/App.jsx
+++ client/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
-export default class App extends React.Component {
-    render() {
-        return (
-            <div>hi</div>
-        )
-    }
-}
+export default () => <div>hi</div>
+
+// export default () => {
+//   return (
+//     <div>hi</div>
+//   )
+// }
+
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <div>hi</div>
+//     )
+//   }
+// }
